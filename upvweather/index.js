8a57742c5062f9a06a5ddac8e376ce4206c0f1a1
develask <develascomikel@gmail.com>
2015-04-24 08:29:04 +0200

    Lekus aldatu

--- upvweather/index.js
+++ upvweather/index.js
@@ -1,4 +1,7 @@
-function makeRequest(method, url, body, callback) {
+function makeRequest(method, url, body, callback, async) {
+    if (!async){
+        async = true;
+    }
     // Obtener la instancia del objeto XMLHttpRequest
     var peticion_http;
     if (window.XMLHttpRequest) {
@@ -19,11 +22,16 @@ function makeRequest(method, url, body, callback) {
     }
 
     // Realizar peticion HTTP
-    peticion_http.open(method, url, true);
+    peticion_http.open(method, url, async);
     peticion_http.send(body);
 }
 var public_key = 'dZaoR0p6pasmppMl5KRd';
 var temperatura = 0.0
+
+makeRequest('GET', 'http://data.sparkfun.com/output/'+public_key+'.json', '', function(data){
+        var data2 = JSON.parse(data);
+        temperatura = data2[0]['temp'];
+}, false);
 
 google.load("visualization", "1", {packages:["gauge"]});
 google.setOnLoadCallback(drawChart);
@@ -45,19 +53,15 @@ var chart = new google.visualization.Gauge(document.getElementById('chart_div'))
 chart.draw(data, options);
 
 setInterval(function() {
-  data.setValue(0, 1, temperatura);
-  chart.draw(data, options);
-}, 500);
+    makeRequest('GET', 'http://data.sparkfun.com/output/'+public_key+'.json', '', function(datos){
+        var datos = JSON.parse(datos);
+        data.setValue(0, 1, datos[0]['temp']);
+        chart.draw(data, options);
+    });
+}, 5000);
 }
 
 document.onreadystatechange = function() {
     if (document.readyState == "complete") {
-        setInterval(function(){
-            makeRequest('GET', 'http://data.sparkfun.com/output/'+public_key+'.json', '', function(data){
-                var data2 = JSON.parse(data);
-                //console.log(data2);
-                temperatura = data2[0]['temp'];
-            });
-        }, 5000);
     }
 }
